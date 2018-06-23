7abd0c153fe87cbc717d4cb0ee03b0ccec1ecf12
akshaysyaduvanshi <akshaysyaduvanshi@gmail.com>
2018-06-24 01:11:35 +0530

    Added Example for AKLabel

--- Example/AKLabel/ViewController.swift
+++ Example/AKLabel/ViewController.swift
@@ -1,24 +1,16 @@
-//
-//  ViewController.swift
-//  AKLabel
-//
-//  Created by akshaysyaduvanshi on 06/23/2018.
-//  Copyright (c) 2018 akshaysyaduvanshi. All rights reserved.
-//
-
 import UIKit
+import AKLabel
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var label: AKLabel!
+	override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		label.animate(text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged", duration: 10, completion: nil)
+	}
 }
 
